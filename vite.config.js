c93fe251982d60067a9a54f1ef72b74e64187928
Your Name <you@example.com>
2025-03-06 20:40:59 +0530

    celebralFrontend

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      fs: 'empty',
-      path: 'empty',
-      crypto: 'empty',
-    },
-  },
-  optimizeDeps: {
-    exclude: ['sql.js'], // Exclude sql.js from dependency optimization
-  },
-  build: {
-    rollupOptions: {
-      external: ['fs', 'path', 'crypto'], // Externalize for compatibility
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-        },
-      },
-    },
-  },
-});
+  plugins: [react(), tailwindcss()],
+  
+})
